75d7f4f87e4b020d70be5c6e5344319e09ab09f7
woshichenghaibo <80568737+woshichenghaibo@users.noreply.github.com>
2024-04-21 13:18:07 +0800

    Update index.js

--- functions/api/bing/wallpaper/index.js
+++ functions/api/bing/wallpaper/index.js
@@ -8,7 +8,8 @@ export async function onRequest(context) {
       next, // used for middleware or to fetch assets
       data, // arbitrary space for passing data between middlewares
     } = context;
-    const res = await fetch(`https://cn.bing.com/HPImageArchive.aspx?format=js&idx=1&n=1`);
+    //const res = await fetch(`https://cn.bing.com/HPImageArchive.aspx?format=js&idx=1&n=1`);
+    const res = await fetch(`/wallpaper.html`);
     const bing_data = await res.json();
     const return_data={
         "status":true,
