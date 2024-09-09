9b647e00780bedae5e914b321b9ab8d50bc1dc17
ohn-max <jsmth3936@gmail.com>
2024-09-09 12:01:39 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -8,11 +8,17 @@ export async function onRequestPost(context) {  // Contents of context object
         next, // used for middleware or to fetch assets    
         data, // arbitrary space for passing data between middlewares 
     } = context;
+    const cookies = request.headers.get('Cookie') || '';
+    const cookiePassword = cookies.split(';').find(cookie => cookie.trim().startsWith('CFP_PASSWORD='));
+    const extractedPassword = cookiePassword ? cookiePassword.split('=')[1].trim() : null;
+    if (extractedPassword !== env.CFP_PASSWORD) {
+        return new Response('Unauthorized: Invalid password', { status: 403 });
+    }
     const clonedRequest = request.clone();
     await errorHandling(context);
     telemetryData(context);
     const url = new URL(clonedRequest.url);
-    const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+    const response = fetch('https://img-proxy.tushangchuan.buzz/' + url.pathname + url.search, {
         method: clonedRequest.method,
         headers: clonedRequest.headers,
         body: clonedRequest.body,
